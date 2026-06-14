f1f230ed42ec32ae3016c30dde559317c19e3862
ame <baibhavghosh03@gmail.com>
2026-06-14 17:32:28 +0530

    Added forecast section

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.uous2erk0v8"
+    "revision": "0.fj7nirfjgj"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
